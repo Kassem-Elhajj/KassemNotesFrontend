d99648e4321e4791eebfac61339829aa739e6f92
Kassem-Elhajj <141503116+Kassem-Elhajj@users.noreply.github.com>
2023-10-07 01:46:11 +0300

    Update login.js

--- src/routes/login.js
+++ src/routes/login.js
@@ -29,7 +29,7 @@ function Login() {
 
     axios.post('https://kassemnotes.onrender.com/auth/login',
       formData,
-      headers: { "Content-Type": "application/json" },
+      {headers: { "Content-Type": "application/json" }},
       {withCredentials: true}
     ).then(res => {
 
