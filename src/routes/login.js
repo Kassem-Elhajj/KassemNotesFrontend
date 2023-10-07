8af0ade2be624e1b9312c4924b4e9edb3a33979e
Kassem-Elhajj <141503116+Kassem-Elhajj@users.noreply.github.com>
2023-10-07 10:55:17 +0300

    Update login.js

--- src/routes/login.js
+++ src/routes/login.js
@@ -29,14 +29,14 @@ function Login() {
 
     axios.post('https://kassemnotes.onrender.com/auth/login',
       formData,
-      { 
-        crossDomain: true,
-        headers: {
-                    "Content-Type": "application/json" ,
-                    Accept: "application/json",
-                    "Access-Control-Allow-Origin": "",
-                 },
-      },
+      // { 
+      //   crossDomain: true,
+      //   headers: {
+      //               "Content-Type": "application/json" ,
+      //               Accept: "application/json",
+      //               "Access-Control-Allow-Origin": "",
+      //            },
+      // },
       {withCredentials: true}
     ).then(res => {
 
@@ -52,9 +52,9 @@ function Login() {
     
   };
 
-  if(navigation){
-    window.location.assign('https://kassemnotesfrontend.onrender.com')
-  }
+  // if(navigation){
+  //   window.location.assign('https://kassemnotesfrontend.onrender.com')
+  // }
 
   if(username){
     return <Navigate to = {'*'} />
